16adf75fd68880f130e14c494da4fb7223543811
HM Tanbir <tanbir2025@gmail.com>
2025-05-08 13:45:02 +0600

    add countryList page and countryDetails page

--- countryInfo/urls.py
+++ countryInfo/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from api import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(urls)),
+    url(r'^api/', include("api.urls")),
+    url(r'^', include("country.urls")),
 ]
